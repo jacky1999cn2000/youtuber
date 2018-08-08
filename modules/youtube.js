e5136576c98bb292cb77086e442ee0348806beb1
Jacky Zhao <liang.zhao83@gmail.com>
2018-08-08 15:15:22 -0700

    set batch to 20, and break down "processed" into 3 different steps: downloaded, converted, watermark

--- modules/youtube.js
+++ modules/youtube.js
@@ -60,7 +60,9 @@ module.exports = async(manifest) => {
     */
     videoList = _.map(videoList, (item) => {
       return {
-        'processed': false,
+        'downloaded': false,
+        'converted': false,
+        'watermark': false,
         'uploaded': false,
         'targetType': media.targetType,
         'targetId': media.targetId,
@@ -127,7 +129,7 @@ module.exports = async(manifest) => {
   let batchList = [];
   // break down finalList to multiple batches (each batch has up to 100 videoIds)
   while (finalList.length) {
-    batchList.push(finalList.splice(0, 50));
+    batchList.push(finalList.splice(0, 20));
   }
   let index = 1;
   for (let batch of batchList) {
